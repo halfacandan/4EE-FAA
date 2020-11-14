aca2164681079e400ae0b5b1be18a0d706602df8
Daniel De Luca <amanofdistinction@lineone.net>
2020-11-14 17:23:05 +0000

    Add Check to prevent bot replying to own messages

--- modules/messages.js
+++ modules/messages.js
@@ -8,9 +8,9 @@ module.exports = {
                     `Monday is a good day to set up your defense teams and buy any Sentinels you want to use, they are effective both for attack and defense. The ${channelOnGwDefence} channel is a great place to discuss your teams or get some inspiration and help.\n\n` +
                     "Using 24 different troops for defence will get you 500 extra points each day you play.\n\n" +
                     `For your attack teams you can use any that you like, but you'll get extra points when you use troops with the colour of the day. There is a wealth of teams posted in the ${channelOnGwOffence} channel. Other things that will have a positive effect on your score are:\n` +
-                    "* How many troops you have on your team when the battle is over\n" +
-                    "* How many damage you did to the opponents troops (devour, lethal and true damage get lower points, AoE gets you more)\n" +
-                    "* How many turns it to win your match, taking a 4/5 match extra turn will count as a new turn\n\n" +    
+                    "  - How many troops you have on your team when the battle is over\n" +
+                    "  - How many damage you did to the opponents troops (devour, lethal and true damage get lower points, AoE gets you more)\n" +
+                    "  - How many turns it to win your match, taking a 4/5 match extra turn will count as a new turn\n\n" +    
                     "That last bullet point can make devour, lethal and true damage troops still very useful. Also, the best score is when you win the match :sweat_smile: so if single colour is too tricky use your favourite team.\n\n" +
                     "Doing your matches on the day will get us a better chance to win the match that day and get all of us extra seals.";
 
